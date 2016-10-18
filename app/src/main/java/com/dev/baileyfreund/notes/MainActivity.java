d678572953d40858b0655b6f928b0be56995e433
baileyfriend <freund.bailey@gmail.com>
2016-10-17 23:03:01 -0400

    Added and edited javadoc with Jamie Penzien

--- app/src/main/java/com/dev/baileyfreund/notes/MainActivity.java
+++ app/src/main/java/com/dev/baileyfreund/notes/MainActivity.java
@@ -1,44 +1,83 @@
 package com.dev.baileyfreund.notes;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.support.v7.app.AlertDialog;
-import android.content.DialogInterface;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.view.View;
 
 import com.dev.baileyfreund.notes.db.NoteContract;
 import com.dev.baileyfreund.notes.db.NoteDbHelper;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.Thing;
 
 import java.util.ArrayList;
 
-import static com.dev.baileyfreund.notes.R.styleable.View;
-
+/**
+ * This class will set up the UI so that way the user can
+ * interact with the app. It allows them to click on buttons,
+ * open the menu, and do things like add a note.
+ *
+ * @author Bailey Freund and Jamie Penzien
+ */
 public class MainActivity extends AppCompatActivity {
+    /**
+     * This string represents a tag for the Main Activity.
+     */
     private static final String TAG = "MainActivity";
+
+    /**
+     * This database helper will assist in forming a data
+     * base out of notes.
+     */
     private NoteDbHelper mHelper;
+
+    /**
+     * This will be the java representation of the TextView in our main activity.
+     */
     private ListView mNoteListView;
+
+    /**
+     * This array adapter will hold an array of strings that
+     * contain information about the layout of the application.
+     */
     private ArrayAdapter<String> mAdapter;
+    /**
+     * This will be the java representation of the TextView in the main activity.
+     */
+    private TextView noteTextView;
+    /**
+     * This will be the java representation of the DeleteButton in the main activity.
+     */
+    private Button deleteButton;
+    /**
+     * This will be the java representation of the Toolbar in the main activity.
+     */
+    private Toolbar toolbar;
 
 
     /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     * This method will set up the app when it is created or
+     * 'opened.' When opened, the app will set the content view,
+     * make a toolbar, and initialize the Database Helper and the
+     * Note List View.
+     *
+     * @param savedInstanceState This saved state is a previous
+     *                           setup of the app that the user
+     *                           had. If there is a saved instance,
+     *                           the app will be set up in that manner
+     *                           using this parameter.
      */
-//    private GoogleApiClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,13 +88,24 @@ public class MainActivity extends AppCompatActivity {
 
         mHelper = new NoteDbHelper(this); //initializing the dbhelper
         mNoteListView = (ListView) findViewById(R.id.list_todo);
+        noteTextView = (TextView) findViewById(R.id.note_title);
+        deleteButton = (Button) findViewById(R.id.note_delete);
 
         updateUI();
     }
 
+
+    /**
+     * This method will update what the user sees on the screen.
+     * It will check to see if there is a new note added to the
+     * database, and if there is it will add it.
+     */
     private void updateUI() {
 
+        //This is the array of the list of notes that is passed to the adapter to be displayed
         ArrayList<String> noteList = new ArrayList<>();
+
+        //This uses the helper for the note database
         SQLiteDatabase db = mHelper.getReadableDatabase();
         Cursor cursor = db.query(NoteContract.NoteEntry.TABLE,
                 new String[]{NoteContract.NoteEntry._ID, NoteContract.NoteEntry.COL_NOTE_TITLE},
@@ -80,15 +130,22 @@ public class MainActivity extends AppCompatActivity {
         cursor.close();
         db.close();
 
+//        int toolbarWidth = toolbar.getWidth();
+//        int delBtnWidth = deleteButton.getWidth();
+//        int noteTextViewWidth = toolbarWidth - delBtnWidth - 5;
+//        noteTextView.setWidth(noteTextViewWidth);
     }
 
-//    public void viewAllNotes(){
-//        Cursor result = mHelper.getAllData();
-//        StringBuffer buffer = new StringBuffer();
-//        while(result.moveToNext()){
-//        }
-//    }
-
+    /**
+     * This method sets up the menu when the app is opened.
+     * It will inflate the menu and add items to the action bar
+     * if it is present.
+     *
+     * @param menu this is the menu that already exists. The
+     *             method will take it and make it appear
+     *             on the screen.
+     * @return this method will return the menu created.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -96,6 +153,16 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * This method will allow a menu to open up and let the user
+     * add a new note from it. It will then add the new note to
+     * the database and make sure the UI is updated.
+     *
+     * @param item This is the new MenuItem being passed to the method;
+     *             the method uses it to make a menu that user can use to add
+     *             a new note.
+     * @return this method will return true if new item has been added to notes.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -129,6 +196,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method will delete a note from the screen and
+     * remove it from the database. It does this by obtaining
+     * the view and then scanning through it to find the
+     * intended note to be deleted. Once deleted, the database
+     * is closed and the UI updated.
+     *
+     * @param view is the current view on the screen that the
+     *             method uses to find which note to be deleted.
+     */
     public void deleteNote(View view) {
         View parent = (View) view.getParent();
         TextView taskTextView = (TextView) parent.findViewById(R.id.note_title);
@@ -141,49 +218,7 @@ public class MainActivity extends AppCompatActivity {
         updateUI();
     }
 
-    /**********
-     * This is currently commented out because we do not yet need the App Indexing API. It is left
-     * because we will eventually add firebase and login from google's API.
-     * But not yet - gotta keep people holding out for something, right?
-     * (really we need time to learn how to do that :P)
-     */
 
-//
-//    /**
-//     * ATTENTION: This was auto-generated to implement the App Indexing API.
-//     * See https://g.co/AppIndexing/AndroidStudio for more information.
-//     */
-//    public Action getIndexApiAction() {
-//        Thing object = new Thing.Builder()
-//                .setName("Main Page") // TODO: Define a title for the content shown.
-//                // TODO: Make sure this auto-generated URL is correct.
-//                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-//                .build();
-//        return new Action.Builder(Action.TYPE_VIEW)
-//                .setObject(object)
-//                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-//                .build();
-//    }
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        //client.connect();
-////        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-////        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-////        client.disconnect();
-//    }
 
 
 }
--- app/src/main/java/com/dev/baileyfreund/notes/db/NoteDbHelper.java
+++ app/src/main/java/com/dev/baileyfreund/notes/db/NoteDbHelper.java
@@ -1,46 +1,66 @@
+
 package com.dev.baileyfreund.notes.db;
 
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 
 /**
- * Created by baileyfreund on 10/10/16.
+ * This class assists with the making of a database that
+ * can help store the information of the notes.
+ *
+ * @author Bailey Freund and Jamie Penzien
  */
-
 public class NoteDbHelper extends SQLiteOpenHelper {
 
-//    public static final String DATABASE_NAME = "note.db";
-//    public static final String TABLE_NAME = "note_table";
-//    public static final String COL_0 = "TITLE";
-//    public static final String COL_1 = "NOTE";
-
-    public NoteDbHelper(Context context){
+    /**
+     * This method is the constructor. Given context, the
+     * constructor will instantiate a NoteContract database
+     * and set its name to null.
+     *
+     * @param context this parameter is the information that
+     *                will be used to establish the database.
+     */
+    public NoteDbHelper(final Context context) {
         super(context, NoteContract.DB_NAME, null, NoteContract.DB_VERSION);
-
     }
 
+    /**
+     * This method sets up the database once the app is
+     * created or opened. It takes a parameter of a database
+     * and then uses that to instantiate the database. This is done
+     * by creating a string of information containing the information
+     * necessary to make a NoteContract database.
+     *
+     * @param db This is the database of information that is going
+     *           to be used to establish the NoteContract database.
+     */
     @Override
-    public void onCreate(SQLiteDatabase db){
-        String createTable = "CREATE TABLE " + NoteContract.NoteEntry.TABLE + " ( " +
-                NoteContract.NoteEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "  +
-                NoteContract.NoteEntry.COL_NOTE_TITLE + " TEXT NOT NULL);";
+    public void onCreate(final SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + NoteContract.NoteEntry.TABLE
+                + " ( " + NoteContract.NoteEntry._ID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + NoteContract.NoteEntry.COL_NOTE_TITLE
+                + " TEXT NOT NULL);";
 
         db.execSQL(createTable);
     }
 
+    /**
+     * This method will take an old version of the database and
+     * update it with a new version which is provided in the parameter.
+     * It will also increase the counting integer that keeps track of
+     * which version the database is.
+     *
+     * @param db this is the new database that will replace the old.
+     * @param oldVersion this is the old version number of the database.
+     * @param newVersion this is the new version number of the database.
+     */
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(final SQLiteDatabase db,
+                          final int oldVersion,
+                          final int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + NoteContract.NoteEntry.TABLE);
         onCreate(db);
     }
-
-//    public Cursor getAllData(){
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        Cursor result = db.rawQuery("select * from " + TABLE_NAME,null);
-//        return result;
-//    }
-
-
 }
